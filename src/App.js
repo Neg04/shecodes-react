daa8404a8b94a288905f2a0fbe42b508a09a6d9a
neg04 <neg.n04@gmail.com>
2023-08-09 17:05:20 +0330

    started react app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      < Weather />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
